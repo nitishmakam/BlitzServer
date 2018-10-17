3649ad4794efb1832a17f2bf6ffd0ef3d631a09c
Rahul Pujari <aecsrahul@gmail.com>
2018-10-17 18:21:21 +0530

    Add few routes

--- routes/questions.js
+++ routes/questions.js
@@ -6,6 +6,7 @@ var User = require('../models/user');
 
 var async = require('async');
 
+// Get all questions
 router.get('/', function(req, res, next) {
     Question.find({})
         .populate({ path: 'user', select: 'username' })
@@ -13,7 +14,69 @@ router.get('/', function(req, res, next) {
         .exec(function (err, questions) {
             if(err) return next(err);
 
-            res.json(questions);
+            return res.json(questions);
+        });
+});
+
+// Get question by id
+router.get('/:id', function(req, res, next) {
+    Question.find({_id: req.params.id})
+        .populate({ path: 'user', select: 'username' })
+        .populate({ path: 'answers.user', select: 'username' })
+        .exec(function (err, question) {
+            if(err) return next(err);
+
+            return res.json(question);
+        });
+});
+
+// Create new question. Takes user(ref) and text
+router.put('/createquestion', function(req, res, next) {
+    var question = new Question({
+        user: req.body.user,
+        text: req.body.text,
+    });
+
+    // validation needed
+    question.save(function (err) {
+        if(err) return next(err);
+
+        return res.json({
+            result: "success",
+        });
+    });
+});
+
+// Create new answer. Takes qid(ref), user(ref) and text
+router.put('/createanswer', function(req, res, next) {
+    Question.findOne({ _id: req.body.qid })
+        .exec(function (err, question) {
+            if(err) return next(err);
+
+            if(!question) {
+                return res.json({
+                    result: "failure",
+                    message: "Question does not exist",
+                });
+            }
+            
+            var answer = {
+                text: req.body.text,
+                user: req.body.user,
+            };
+
+            if(question.answers === undefined)
+                question.answers = [answer];
+            else
+                question.answers.push(answer);
+
+            question.save(function (err) {
+                if(err) return next(err);
+
+                return res.json({
+                    result: "success",
+                });
+            });
         });
 });
 
