28622914e52275c22d0a7e0d5445cd9730c922e1
nitishmakam <nitishmakam@gmail.com>
2018-11-07 19:15:35 +0530

    username validity

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 router.get('/usernameValid/:username', function (req, res, next) {
-  var username = req.params.username;
-  //Write logic to query database and check if the username exists
-  if (username === "nope") {
-    res.status(403).send();
-  }
-  else {
-    res.status(200).send();
-  }
+  User.find({ username: req.params.username })
+    .exec(function (err, user) {
+      console.log(user);
+      if (err) {
+        res.status(403).send();
+      } else {
+        if (user.length == 0) {
+          res.status(200).send();
+        }
+        else {
+          res.status(403).send();
+        }
+      }
+    });
 });
 
 module.exports = router;
